fb5db1777751f7fd45972c4ff72802295e6cb954
shuangxi <double8295@gmail.com>
2025-08-02 23:22:21 +0700

    feat: fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -33,12 +33,54 @@ export default defineConfig({
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'Vue3TiptapEditor',
             fileName: (format) => `index.${format}.js`,
-            formats: ['es','umd','iife']
+            formats: ['es']
         },
         rollupOptions: {
-            external: ['vue'], // 避免重复打包 vue
+            external: [
+                'vue',
+                'vue',
+                '@tiptap/core',
+                '@tiptap/vue-3',
+                '@tiptap/starter-kit',
+                '@tiptap/pm',
+                '@tiptap/extension-link',
+                '@tiptap/extension-image',
+                '@tiptap/extension-floating-menu',
+                '@tiptap/extension-bubble-menu',
+                '@tiptap/extension-task-item',
+                '@tiptap/extension-task-list',
+                '@tiptap/extension-color',
+                '@tiptap/extension-text-style',
+                '@tiptap/extension-text-align',
+                '@tiptap/extension-underline',
+                '@tiptap/extension-highlight',
+                '@tiptap/extension-subscript',
+                '@tiptap/extension-superscript',
+                '@vueuse/core',
+                '@tiptap/extension-list-item',
+            ], // 避免重复打包 vue
             output: {
-                globals: { vue: 'Vue' }
+                globals: { 
+                    vue: 'Vue',
+                    '@vueuse/core': 'VueUse',
+                    '@tiptap/core': 'TiptapCore',
+                    '@tiptap/starter-kit': 'StarterKit',
+                    '@tiptap/vue-3': 'TiptapVue',
+                    '@tiptap/extension-bubble-menu': 'BubbleMenu',
+                    '@tiptap/extension-color': 'Color',
+                    '@tiptap/extension-floating-menu': 'FloatingMenu',
+                    '@tiptap/extension-highlight': 'Highlight',
+                    '@tiptap/extension-image': 'Image',
+                    '@tiptap/extension-link': 'Link',
+                    '@tiptap/extension-list-item': 'ListItem',
+                    '@tiptap/extension-subscript': 'Subscript',
+                    '@tiptap/extension-superscript': 'Superscript',
+                    '@tiptap/extension-task-item': 'TaskItem',
+                    '@tiptap/extension-task-list': 'TaskList',
+                    '@tiptap/extension-text-align': 'TextAlign',
+                    '@tiptap/extension-text-style': 'TextStyle',
+                    '@tiptap/extension-underline': 'Underline',
+                }
             }
         }
     },
